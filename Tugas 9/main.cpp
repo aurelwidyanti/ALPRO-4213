deff123080a4ed34290dfc1b02e823f90f20c363
aurelwidyanti <128146598+aurelwidyanti@users.noreply.github.com>
2023-05-27 22:12:17 +0700

    Add files via upload

---
+++ Tugas 9/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int penjumlahan (int a, int b);
+int pengurangan (int a, int b);
+int perkalian (int a, int b);
+int pembagian (int a, int b);
+int pangkat (int a, int b);
+
+int penjumlahan (int a, int b){
+    if (a <= 0 && b <= 0)
+        return 0;
+    else if (a > 0)
+        return 1 + penjumlahan (a - 1, b);
+    else if (b > 0)
+        return 1 + penjumlahan (a, b - 1);
+}
+
+int pengurangan (int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return pengurangan (a - 1, b - 1);
+    }
+}
+
+int perkalian (int a, int b) {
+    if (a == 0 || b == 0) {
+        return 0;
+    } else {
+        return a + perkalian (a, b - 1);
+    }
+}
+
+int pembagian (int a, int b) {
+    if (a < b) {
+        return 0;
+    } else {
+        return 1 + pembagian (a - b, b);
+    }
+}
+
+int pangkat (int a, int b) {
+    if (b == 0) {
+        return 1;
+    } else {
+        return a * pangkat (a, b - 1);
+    }
+}
+
+
+int main()
+{
+    cout << penjumlahan (5,7) << endl;
+    cout << pengurangan (10,6) << endl;
+    cout << perkalian (5,8) << endl;
+    cout << pembagian (15,3) << endl;
+    cout << pangkat (3,4) << endl;
+    return 0;
+}
