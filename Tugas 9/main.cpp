82f44e404ae4181217447e179bd281569bd44037
aurelwidyanti <128146598+aurelwidyanti@users.noreply.github.com>
2023-05-28 16:41:08 +0700

    Update main.cpp

--- Tugas 9/main.cpp
+++ Tugas 9/main.cpp
@@ -2,13 +2,7 @@
 
 using namespace std;
 
-int penjumlahan (int a, int b);
-int pengurangan (int a, int b);
-int perkalian (int a, int b);
-int pembagian (int a, int b);
-int pangkat (int a, int b);
-
-int penjumlahan (int a, int b){
+int penjumlahan (int a, int b) {
     if (a <= 0 && b <= 0)
         return 0;
     else if (a > 0)
@@ -18,44 +12,47 @@ int penjumlahan (int a, int b){
 }
 
 int pengurangan (int a, int b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return pengurangan (a - 1, b - 1);
-    }
+    if (a <= 0 && b <= 0)
+        return 0;
+    else if (a > 0)
+        return 1 + pengurangan (a - 1, b);
+    else if (b > 0)
+        return -1 + pengurangan (a, b - 1);
 }
 
 int perkalian (int a, int b) {
-    if (a == 0 || b == 0) {
+    if (a == 0 || b == 0)
         return 0;
-    } else {
+    else if (a > 0)
+        return b + perkalian (a - 1, b);
+    else if (b > 0)
         return a + perkalian (a, b - 1);
-    }
 }
 
 int pembagian (int a, int b) {
-    if (a < b) {
+    if (a < b)
         return 0;
-    } else {
+    else if (a == b)
+        return 1;
+    else
         return 1 + pembagian (a - b, b);
-    }
 }
 
 int pangkat (int a, int b) {
-    if (b == 0) {
+    if (a == 0)
+        return 0;
+    else if (b == 0)
         return 1;
-    } else {
+    else if (a > 0)
         return a * pangkat (a, b - 1);
-    }
 }
-
 
 int main()
 {
     cout << penjumlahan (5,9) << endl;
-    cout << pengurangan (10,3) << endl;
-    cout << perkalian (5,5) << endl;
-    cout << pembagian (30,3) << endl;
-    cout << pangkat (5,4) << endl;
+    cout << pengurangan (8,9) << endl;
+    cout << perkalian (5,0) << endl;
+    cout << pembagian (9,3) << endl;
+    cout << pangkat (0,2) << endl;
     return 0;
 }
